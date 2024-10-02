51c660b0d271e5d3646b5dfb07bc780dfb730a1c
IT24102375 <it24102375@my.sliit.lk>
2024-10-02 08:07:55 +0530

    IT24102375Lab7Q2A

---
+++ Q2/IT24102375Lab7Q2A.java
@@ -0,0 +1,18 @@
+public class IT24102375Lab7Q2A
+{
+ public static void main(String[] args)
+{
+  for(int i=0;i<4;i++)
+ {
+   for(int j=0;j<4;j++)
+   {
+      System.out.print("$");
+   }
+ System.out.print("\n");
+ }
+
+
+
+}
+
+}
